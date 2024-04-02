a494bc8af292bfe7f6ae4fbe9b8a5155ac340ca3
mightymjolnir <mightymjolnir@qq.com>
2024-04-02 21:03:52 +0800

    增加注释

--- dataset/tum_rgbd.py
+++ dataset/tum_rgbd.py
@@ -1,17 +1,23 @@
-import torch
+"""
+加载数据集
+"""
+
 from os import path
-from tqdm import tqdm
-import imageio
+
 import cv2
+import imageio
 import numpy as np
 import open3d as o3d
+import torch
+from tqdm import tqdm
 
 
 def get_calib():
+    # fx, fy, cx, cy
     return {
         "fr1": [517.306408, 516.469215, 318.643040, 255.313989],
         "fr2": [520.908620, 521.007327, 325.141442, 249.701764],
-        "fr3": [535.4, 539.2, 320.1, 247.6]
+        "fr3": [535.4, 539.2, 320.1, 247.6],
     }
 
 
@@ -23,14 +29,18 @@ def load_K_Rt_from_P(P):
     out = cv2.decomposeProjectionMatrix(P)
     K = out[0]
     R = out[1]
-    t = out[2]
+    t = out[2]  # 4x1
 
-    K = K/K[2,2]
+    K = K / K[2, 2]
     intrinsics = np.eye(4)
     intrinsics[:3, :3] = K
 
     pose = np.eye(4, dtype=np.float32)
+    # 旋转矩阵 R 为正交矩阵，R^{-1} = R^T
     pose[:3, :3] = R.transpose()  # convert from w2c to c2w
+    # 出于某种原因，位移 t 计算方法为 - t[:3] / t[3]
+    # see: https://stackoverflow.com/questions/62686618/opencv-decompose-projection-matrix
+    # 这里由于 w2c -> c2w，取逆，则位移取负数
     pose[:3, 3] = (t[:3] / t[3])[:, 0]
 
     return intrinsics, pose
@@ -42,14 +52,14 @@ class TUMDataset(torch.utils.data.Dataset):
     """
 
     def __init__(
-            self,
-            rootdir,
-            device,
-            near: float = 0.2,
-            far: float = 5.,
-            img_scale: float = 1.,  # image scale factor
-            start: int = -1,
-            end: int = -1,
+        self,
+        rootdir,
+        device,
+        near: float = 0.2,
+        far: float = 5.0,
+        img_scale: float = 1.0,  # image scale factor
+        start: int = -1,
+        end: int = -1,
     ):
         super().__init__()
         assert path.isdir(rootdir), f"'{rootdir}' is not a directory"
@@ -84,20 +94,32 @@ class TUMDataset(torch.utils.data.Dataset):
             # read images
             rgb = np.array(imageio.imread(path.join(data_path, "rgb/{:04d}.png".format(i)))).astype(np.float32)
             depth = np.array(imageio.imread(path.join(data_path, "depth/{:04d}.png".format(i)))).astype(np.float32)
-            depth /= 5000.  # TODO: put depth factor to args
+
+            # NOTE: 深度值除以一个因数
+            depth /= 5000.0  # TODO: put depth factor to args
             d_max += [depth.max()]
             d_min += [depth.min()]
             # depth = cv2.bilateralFilter(depth, 5, 0.2, 15)
             # print(depth[depth > 0.].min())
+
+            # ray-casting 对深度区间进行限制
+            # 这里把 [near, far] 区间外的深度值都设为 -1.0，作为不可见区域
             invalid = (depth < near) | (depth > far)
-            depth[invalid] = -1.
+            depth[invalid] = -1.0  # NOTE: 我个人感觉，截断的地方深度值直接设为 0 就好了，后续处理时过滤掉了 depth=0 的像素
+
             # downscale the image size if needed
             if img_scale < 1.0:
-                full_size = list(rgb.shape[:2])
+                full_size = list(rgb.shape[:2])  # H, W
+                # 计算缩小后的图像尺寸，使用四舍五入获得整数
                 rsz_h, rsz_w = [round(hw * img_scale) for hw in full_size]
+
                 # TODO: figure out which way is better: skimage.rescale or cv2.resize
-                rgb = cv2.resize(rgb, (rsz_w, rsz_h), interpolation=cv2.INTER_AREA)
+                # 缩小图片 see: https://zhuanlan.zhihu.com/p/38493205
+                rgb = cv2.resize(rgb, (rsz_w, rsz_h), interpolation=cv2.INTER_AREA)  # 使用像素区域关系进行重采样
+                # 对深度图采用最近邻插值，感觉不使用线性插值是因为边界处线性插值获得的深度是没有意义的。
                 depth = cv2.resize(depth, (rsz_w, rsz_h), interpolation=cv2.INTER_NEAREST)
+
+                # NOTE: 相机内参会随着图像尺寸同比缩小
                 intrinsics[0, 0] *= img_scale
                 intrinsics[1, 1] *= img_scale
                 intrinsics[0, 2] *= img_scale
@@ -116,8 +138,13 @@ class TUMDataset(torch.utils.data.Dataset):
         return self.n_images
 
     def __getitem__(self, idx):
-        return self.rgb_all[idx].to(self.device), self.depth_all[idx].to(self.device), \
-               self.c2w_all[idx].to(self.device), self.K_all[idx].to(self.device)
+        "rgb, depth, c2w (相机位姿), K (内参)"
+        return (
+            self.rgb_all[idx].to(self.device),
+            self.depth_all[idx].to(self.device),
+            self.c2w_all[idx].to(self.device),
+            self.K_all[idx].to(self.device),
+        )
 
 
 class TUMDatasetOnline(torch.utils.data.Dataset):
@@ -126,14 +153,14 @@ class TUMDatasetOnline(torch.utils.data.Dataset):
     """
 
     def __init__(
-            self,
-            rootdir,
-            device,
-            near: float = 0.2,
-            far: float = 5.,
-            img_scale: float = 1.,  # image scale factor
-            start: int = -1,
-            end: int = -1,
+        self,
+        rootdir,
+        device,
+        near: float = 0.2,
+        far: float = 5.0,
+        img_scale: float = 1.0,  # image scale factor
+        start: int = -1,
+        end: int = -1,
     ):
         super().__init__()
         assert path.isdir(rootdir), f"'{rootdir}' is not a directory"
@@ -185,10 +212,13 @@ class TUMDatasetOnline(torch.utils.data.Dataset):
         # read images
         rgb = np.array(imageio.imread(self.rgb_files_all[idx])).astype(np.float32)
         depth = np.array(imageio.imread(self.depth_files_all[idx])).astype(np.float32)
-        depth /= 5000.
+        depth /= 5000.0
+        # NOTE: TODO: 目前这个双边滤波看起来效果并不明显…… 如果有时间可以研究下参数设定
         # depth = cv2.bilateralFilter(depth, 5, 0.2, 15)
-        depth[depth < self.near] = 0.
-        depth[depth > self.far] = -1.
+
+        # ray-casting 对深度区间进行限制
+        depth[depth < self.near] = 0.0
+        depth[depth > self.far] = -1.0
         # downscale the image size if needed
         if self.img_scale < 1.0:
             full_size = list(rgb.shape[:2])
@@ -204,4 +234,3 @@ class TUMDatasetOnline(torch.utils.data.Dataset):
         depth = torch.from_numpy(depth).to(self.device)
 
         return rgb, depth, c2w, K
-
