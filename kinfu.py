cec226901306cc58f2cea2d019f64cffc0b6846c
mightymjolnir <mightymjolnir@qq.com>
2024-04-10 17:21:30 +0800

    增加注释

--- kinfu.py
+++ kinfu.py
@@ -1,26 +1,29 @@
-import os
 import argparse
+import os
+
+import cv2
 import numpy as np
 import torch
-import cv2
-import trimesh
-from matplotlib import pyplot as plt
-from fusion import TSDFVolumeTorch
-from dataset.tum_rgbd import TUMDataset, TUMDatasetOnline
-from tracker import ICPTracker
-from utils import load_config, get_volume_setting, get_time
 
+# 与 kinfu_gui.py 相比，没有使用 open3d，而是使用了下面的两个包
+import trimesh  # 处理 triangular meshes 的包，这里主要用于导出 mesh.ply
+from matplotlib import pyplot as plt
+
+from dataset.tum_rgbd import TUMDataset, TUMDatasetOnline
+from fusion import TSDFVolumeTorch
+from tracker import ICPTracker
+from utils import get_time, get_volume_setting, load_config
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # standard configs
-    parser.add_argument('--config', type=str, default="configs/fr1_desk.yaml", help='Path to config file.')
+    parser.add_argument("--config", type=str, default="configs/fr1_desk.yaml", help="Path to config file.")
     parser.add_argument("--save_dir", type=str, default=None, help="Directory of saving results.")
     args = load_config(parser.parse_args())
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # device = torch.device("cpu")
-    dataset = TUMDataset(os.path.join(args.data_root), device, near=args.near, far=args.far, img_scale=0.25)
+    dataset = TUMDatasetOnline(os.path.join(args.data_root), device, near=args.near, far=args.far, img_scale=0.25)
     H, W = dataset.H, dataset.W
 
     vol_dims, vol_origin, voxel_size = get_volume_setting(args)
@@ -39,17 +42,14 @@ if __name__ == "__main__":
             curr_pose = pose_gt
         else:  # tracking
             # 1. render depth image (1) from tsdf volume
-            depth1, color1, vertex01, normal1, mask1 = tsdf_volume.render_model(curr_pose, K, H, W, near=args.near, far=args.far, n_samples=args.n_steps)
+            depth1, color1, vertex01, normal1, mask1 = tsdf_volume.render_model(
+                curr_pose, K, H, W, near=args.near, far=args.far, n_samples=args.n_steps
+            )
             T10 = icp_tracker(depth0, depth1, K)  # transform from 0 to 1
             curr_pose = curr_pose @ T10
 
         # fusion
-        tsdf_volume.integrate(depth0,
-                              K,
-                              curr_pose,
-                              obs_weight=1.,
-                              color_img=color0
-                              )
+        tsdf_volume.integrate(depth0, K, curr_pose, obs_weight=1.0, color_img=color0)
         t1 = get_time()
         t += [t1 - t0]
         print("processed frame: {:d}, time taken: {:f}s".format(i, t1 - t0))
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         poses_gt += [pose_gt.cpu().numpy()]
 
     avg_time = np.array(t).mean()
-    print("average processing time: {:f}s per frame, i.e. {:f} fps".format(avg_time, 1. / avg_time))
+    print("average processing time: {:f}s per frame, i.e. {:f} fps".format(avg_time, 1.0 / avg_time))
     # compute tracking ATE
     poses_gt = np.stack(poses_gt, 0)
     poses = np.stack(poses, 0)
@@ -80,4 +80,3 @@ if __name__ == "__main__":
         partial_tsdf.export(os.path.join(args.save_dir, "mesh.ply"))
         np.savez(os.path.join(args.save_dir, "traj.npz"), poses=poses)
         np.savez(os.path.join(args.save_dir, "traj_gt.npz"), poses=poses_gt)
-
