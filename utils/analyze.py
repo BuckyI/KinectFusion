a48f02ad9a422f77ffe627731bd1339cefa50772
mightymjolnir <mightymjolnir@qq.com>
2024-04-13 22:06:51 +0800

    ✨ feat(display): 可视化重建结果输出

--- utils/analyze.py
+++ utils/analyze.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import open3d as o3d
 import pandas as pd
 
 
@@ -18,3 +19,11 @@ def profile(path: str = "perf_report.txt"):
     data = [_.split(maxsplit=len(headers) - 1) for _ in profile[1:]]
     df = pd.DataFrame(data, columns=headers)
     df.to_csv("perf_report.csv", index=False)
+
+
+def display(path: str):
+    "display construction result, saved as .ply file"
+    assert Path(path).exists()
+    # pcd = o3d.io.read_point_cloud(path) # 这个也可以
+    mesh = o3d.io.read_triangle_mesh(path)
+    o3d.visualization.draw_geometries([mesh])
